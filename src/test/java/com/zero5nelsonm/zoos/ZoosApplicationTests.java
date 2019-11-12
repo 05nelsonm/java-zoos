1bd3bc2ca767638c7fb6cfac5377a277e8cd7c0a
Matthew Nelson <05nelsonm@protonmail.com>
2019-11-11 19:25:09 -0500

    initialized IntelliJ project

---
+++ src/test/java/com/zero5nelsonm/zoos/ZoosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero5nelsonm.zoos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
